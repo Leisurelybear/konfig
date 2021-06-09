c7f861464f70df48dff6b5f0053e9a180bfce305
jason <jasonzhang666@163.com>
2021-06-09 22:03:42 +0800

    - 配置maven打包：在父项目中使用 mvn clean package -Dmaven.test.skip=true 可以进行编译，把每个模块打包成jar到target目录，再把对应的yml配置文件放到target中，就可以通过 java -jar file.jar 运行了
    - mvn clean package -Dmaven.test.skip=true
    - java -jar konfig-authentication-1.0-SNAPSHOT.jar
    - java -jar konfig-configservice-1.0-SNAPSHOT.jar

--- konfig-authentication-8021/src/main/java/org/zhangxujie/konfig/ConfigAuthMain8021.java
+++ konfig-authentication-8021/src/main/java/org/zhangxujie/konfig/ConfigAuthMain8021.java
@@ -2,11 +2,13 @@ package org.zhangxujie.konfig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.WebApplicationInitializer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class ConfigAuthMain8021 {
+public class ConfigAuthMain8021 extends SpringBootServletInitializer implements WebApplicationInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigAuthMain8021.class, args);
--- konfig-configservice-8301/src/test/java/T1.java
+++ konfig-configservice-8301/src/test/java/T1.java
@@ -15,31 +15,31 @@ public class T1 {
     @Test
     public void t1() {
         //TODO:在8021添加检查是否有用户的接口
-        String res = HttpRequest
-                .post("http://localhost:8301/config/list?token=eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJyb290IiwiY3JlYXRlZCI6MTYxODQ5OTU5OTgyMCwiZXhwIjoxNjE5MTA0Mzk5fQ.WnlfxEko3a-An3GrEP_ikEyzYl0O_RsbbvjPZSPIUBj7j6Mqhy009DSU4BGgN5oBtKH7S8Ja27SGrJ6EniXw0A")
-                .body("{\"collectionIds\":[\"6\"],\"nameLike\":\"\",\"keyLike\":\"\",\"sort\":1}")
-                .timeout(20000)
-                .execute().body();
-        res = XML.toJSONObject(res).get("CommonResult").toString();
-        Console.log(res);
+//        String res = HttpRequest
+//                .post("http://localhost:8301/config/list?token=eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJyb290IiwiY3JlYXRlZCI6MTYxODQ5OTU5OTgyMCwiZXhwIjoxNjE5MTA0Mzk5fQ.WnlfxEko3a-An3GrEP_ikEyzYl0O_RsbbvjPZSPIUBj7j6Mqhy009DSU4BGgN5oBtKH7S8Ja27SGrJ6EniXw0A")
+//                .body("{\"collectionIds\":[\"6\"],\"nameLike\":\"\",\"keyLike\":\"\",\"sort\":1}")
+//                .timeout(20000)
+//                .execute().body();
+//        res = XML.toJSONObject(res).get("CommonResult").toString();
+//        Console.log(res);
     }
 
     @Test
     public void hasUser() {
-        String token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJyb290IiwiY3JlYXRlZCI6MTYxODQ5OTU5OTgyMCwiZXhwIjoxNjE5MTA0Mzk5fQ.WnlfxEko3a-An3GrEP_ikEyzYl0O_RsbbvjPZSPIUBj7j6Mqhy009DSU4BGgN5oBtKH7S8Ja27SGrJ6EniXw0A";
-        String res = HttpRequest
-                .get("http://localhost:8021/admin/info")
-                .header("Authorization", "Bearer " + token)
-                .timeout(20000)
-                .execute().body();
-
-        JSONObject jsonObj = XML.toJSONObject(res);
-        if (jsonObj != null){
-            res = jsonObj.getByPath("CommonResult.data.username").toString();
-        }else {
-            res = "";
-        }
-        Console.log(res);
+//        String token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJyb290IiwiY3JlYXRlZCI6MTYxODQ5OTU5OTgyMCwiZXhwIjoxNjE5MTA0Mzk5fQ.WnlfxEko3a-An3GrEP_ikEyzYl0O_RsbbvjPZSPIUBj7j6Mqhy009DSU4BGgN5oBtKH7S8Ja27SGrJ6EniXw0A";
+//        String res = HttpRequest
+//                .get("http://localhost:8021/admin/info")
+//                .header("Authorization", "Bearer " + token)
+//                .timeout(20000)
+//                .execute().body();
+//
+//        JSONObject jsonObj = XML.toJSONObject(res);
+//        if (jsonObj != null){
+//            res = jsonObj.getByPath("CommonResult.data.username").toString();
+//        }else {
+//            res = "";
+//        }
+//        Console.log(res);
     }
 
 
